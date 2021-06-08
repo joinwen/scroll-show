d3c048de83baa23ed5a15719cd2bb7ee48459914
cywen <18205055638@163.com>
2021-06-08 19:27:15 +0800

    init

---
+++ src/tools/index.js
@@ -0,0 +1,77 @@
+/**
+ * 获取 dom 属性
+ */
+const getStyle = (ele, attr) => {
+  return document.defaultView.getComputedStyle(ele)[attr];
+}
+
+/**
+ * 字符串转数字
+ */
+const toNumber = (str) => {
+  return Number.parseInt(str);
+}
+
+/**
+ * 获取 高度
+ */
+const getHeight = (ele) => {
+  let height = getStyle(ele,"height");
+  return toNumber(height);
+}
+
+/**
+ * 获取 高度 + 内边距
+ */
+const getHeightWithPadding = (ele) => {
+  return ele.clientHeight;
+}
+
+/**
+ * 获取 高度 + 内边距 + 边框
+ */
+const getHeightWithBorder = (ele) => {
+  return ele.offsetHeight;
+}
+
+/**
+ * 获取 高度 + 内边距 + 边框 + 外边距
+ */
+const getHeightWithMargin = (ele) => {
+  let height = getHeightWithBorder(ele),
+    mt = getStyle(ele, "margin-top"),
+    mb = getStyle(ele, "margin-bottom");
+  return height + mt + mb;
+}
+
+/**
+ * 获取宽度
+ */
+const getWidth = (ele) => {
+  let width = getStyle(ele, "width");
+  return toNumber(width);
+}
+
+/**
+ * 获取宽度 + 内边距
+ */
+const getWidthWithPadding = (ele) => {
+  return ele.clientWidth;
+}
+
+/**
+ * 获取 宽度 + 内边距 + 边框
+ */
+const getWidthWithBorder = (ele) => {
+  return ele.offsetWidth;
+}
+
+/**
+ * 获取 宽度 + 内边距 + 边框 + 外边距
+ */
+const getWidthWithMargin = (ele) => {
+  let width = getWidthWithBorder(ele),
+    ml = getStyle(ele, "margin-left"),
+    mr = getStyle(ele,"margin-right");
+  return width + ml + mr;
+}
