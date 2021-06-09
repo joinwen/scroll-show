aadaf8043073743d465059217da03e6972797c06
cywen <18205055638@163.com>
2021-06-09 15:15:51 +0800

    dev: 检测 y 值

--- src/core/index.js
+++ src/core/index.js
@@ -2,21 +2,41 @@ import {clientY, getHeightWithMargin, getStyle, pageY, toNumber} from "../tools/
 
 const defaultOptions = {
   wrapped: null,
-  child: null
+  child: null,
+  border: true,
+  cb: () => {}
 }
 const removeBorder = (wrapped, y) => {
   let bt = getStyle(wrapped, "border-top");
   return y - toNumber(bt);
 }
 
+const judgeY = (y, options) => {
+  let wrapped = options.wrapped,
+    child = options.child,
+    callback = options.cb,
+    border = options.border;
+  if(y <= 0) {
+    let newY = y + (border ? child.offsetHeight : child.clientHeight);
+    console.log(newY);
+    if(newY > 0) {
+      callback(child, true);
+    } else {
+      callback(child, false);
+    }
+  } else {
+    if(y <= wrapped.clientHeight && y >= 0) {
+      callback(child, true);
+    } else {
+      callback(child, false);
+    }
+  }
+}
 const scrollShow = (options) => {
   options = Object.assign(defaultOptions, options);
-  let wrapped = options.wrapped,
-    child = options.child;
-  let y = clientY(wrapped, child),
-    childHeight = getHeightWithMargin(child);
-  y = removeBorder(wrapped, y);
-  return [y, childHeight];
+  let y = clientY(options.wrapped, options.child);
+  y = removeBorder(options.wrapped, y);       // wrapped 元素的 border 宽度会计算进去，在这里减掉
+  judgeY(y, options);
 }
 export default scrollShow;
 
